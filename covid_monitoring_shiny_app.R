9f24172e9abeadd370ed1ca7e0cb5aad7d14327f
Estelle Ou <estelle0518@gmail.com>
2022-03-30 23:02:08 -0400

    created first draft of first webpage of case and death hotspot

--- covid_monitoring_shiny_app.R
+++ covid_monitoring_shiny_app.R
@@ -75,6 +75,196 @@ cntry_cleaned_covid <-
 source(paste0(wd, "/Rscripts/country_classification.R"))
 
 
+#functions for manupulating data -------------------
+
+hotspot <- function(region, ranking_type) {
+  
+  increases_in_deaths_and_cases_within_this_week <- 
+    cntry_cleaned_covid %>% 
+    group_by(country) %>% 
+    mutate(change_7day_avg_new_cases_per_pop = new_cases_avg_per_pop-lag(new_cases_avg_per_pop),
+           change_7day_avg_new_deaths_per_pop = new_deaths_avg_per_pop-lag(new_deaths_avg_per_pop),
+           avg_chg_weeklycases = rollmean(change_7day_avg_new_cases_per_pop, k = 14, align = "right", fill = NA),
+           avg_chg_weeklydeaths = rollmean(change_7day_avg_new_deaths_per_pop, k = 14, align = "right", fill = NA),) %>% 
+    filter(date == last(date)) %>%  
+    ungroup() 
+  
+  if (ranking_type=="deaths") {
+    
+    hotspot <- 
+      increases_in_deaths_and_cases_within_this_week %>%
+      arrange(desc(avg_chg_weeklydeaths)) 
+    
+    if (region == "global") {
+      data <- 
+        hotspot %>% 
+        slice_head(n = 20)
+      
+    } else {
+      
+      data <- 
+        hotspot %>% 
+        filter(country %in% region)
+      
+    }
+    
+  } else { 
+    
+    hotspot <- 
+      increases_in_deaths_and_cases_within_this_week %>%
+      arrange(desc(avg_chg_weeklycases)) 
+    
+    if (region == "global") {
+      data <- 
+        hotspot %>% 
+        slice_head(n = 20)
+      
+    } else {
+      
+      data <- 
+        hotspot %>% 
+        filter(country %in% region)
+      
+    }
+    
+  }
+  
+  return(data)
+}
+
+
+#visual ranking of hotspots
+cases_hotspot_visuals <- function(region) {
+  
+  ranked_data <- 
+    hotspot(region, "cases") %>% 
+    select(country, avg_chg_weeklycases) 
+  
+  ranked_list <- 
+    unique(ranked_data$country)
+  
+  hot <- 
+    ranked_data %>% 
+    mutate(country = fct_rev(factor(country, levels = ranked_list))) %>% 
+    filter(country != "NA") %>% 
+    mutate(avg_chg_weeklycases = ifelse(avg_chg_weeklycases >0, avg_chg_weeklycases, 0))
+  
+  cool <- 
+    ranked_data %>% 
+    mutate(country = fct_rev(factor(country, levels = ranked_list))) %>% 
+    filter(country != "NA") %>% 
+    mutate(avg_chg_weeklycases = ifelse(avg_chg_weeklycases <0, avg_chg_weeklycases, 0))
+  
+  
+  ggplot() +
+    geom_bar(data = hot, aes(y= country, x = avg_chg_weeklycases, fill = avg_chg_weeklycases>0),
+             stat = "identity", fill = "red") +
+    geom_bar(data = cool, aes(y= country, x = avg_chg_weeklycases, fill = avg_chg_weeklycases<=0),
+             stat = "identity", fill = "grey") +
+    geom_vline(xintercept = 0)+
+    labs(x= "", y= "", 
+         subtitle = paste0("Change in Weekly Average New Cases Per Million (14-day avg.) \nAs of ", today()-1)) +
+    theme_bw()+
+    theme(
+      plot.margin = margin(0, 1, 0.5, 1, unit = "cm"),
+      plot.subtitle= element_text(size = 16),
+      plot.title = element_text(size = 16),
+      axis.text = element_text(face = "bold", size = 16),
+      axis.text.y = element_text(face = "bold", size = 16,
+                                 color = as.vector((rev(ifelse(ranked_data$avg_chg_weeklycases >= 0, "red", "black"))))))
+}
+
+#visual ranking of hotspots
+death_hotspot_visuals <- function(region) {
+  
+  ranked_data <- 
+    hotspot(region, "deaths") %>% 
+    select(country, avg_chg_weeklydeaths) 
+  
+  ranked_list <- 
+    unique(ranked_data$country)
+  
+  hot <- 
+    ranked_data %>% 
+    mutate(country = fct_rev(factor(country, levels = ranked_list))) %>% 
+    filter(country != "NA") %>% 
+    mutate(avg_chg_weeklydeaths = ifelse(avg_chg_weeklydeaths >0, avg_chg_weeklydeaths, 0))
+  
+  cool <- 
+    ranked_data %>% 
+    mutate(country = fct_rev(factor(country, levels = ranked_list))) %>% 
+    filter(country != "NA") %>% 
+    mutate(avg_chg_weeklydeaths = ifelse(avg_chg_weeklydeaths <0, avg_chg_weeklydeaths, 0))
+  
+  
+  ggplot() +
+    geom_bar(data = hot, aes(y= country, x = avg_chg_weeklydeaths, fill = avg_chg_weeklydeaths>0),
+             stat = "identity", fill = "red") +
+    geom_bar(data = cool, aes(y= country, x = avg_chg_weeklydeaths, fill = avg_chg_weeklydeaths<=0),
+             stat = "identity", fill = "grey") +
+    geom_vline(xintercept = 0)+
+    labs(x= "", y= "", 
+         subtitle = paste0("Change in Weekly Average New Deaths Per Million (14-day avg.) \nAs of ", today()-1)) +
+    theme_bw()+
+    theme(
+      plot.margin = margin(0, 1, 0.5, 1, unit = "cm"),
+      plot.subtitle= element_text(size = 16),
+      plot.title = element_text(size = 16),
+      axis.text = element_text(face = "bold", size = 16),
+      axis.text.y = element_text(face = "bold", size = 16,
+                                 color = as.vector((rev(ifelse(ranked_data$avg_chg_weeklydeaths >= 0, "red", "black"))))))
+}
+
+#cache all the graphs to optimize refresh rate ---------------------------------
+em_case_hotspot <-
+  cases_hotspot_visuals(em) +
+  labs(title = "EM Covid Case Hotspots")
+
+dm_case_hotspot <-
+  cases_hotspot_visuals(dm) +
+  labs(title = "DM Covid Case Hotpots")
+
+eu_case_hotspot <-
+  cases_hotspot_visuals(eu) +
+  labs(title = "EU Covid Case Hotspots")
+
+asia_case_hotspot <-
+  cases_hotspot_visuals(asia)+
+  labs(title = "Asia Covid Case Hotspots")
+
+latam_case_hotspot <-
+  cases_hotspot_visuals(latam) +
+  labs(title = "Latam Covid Case Hotspots")
+
+global_case_hotspot <-
+  cases_hotspot_visuals("global") +
+  labs(title = "Top 20 Global Covid Case Hotspots")
+
+em_death_hotspot <-
+  death_hotspot_visuals(em) +
+  labs(title = "EM Covid Death Hotspots")
+
+dm_death_hotspot <-
+  death_hotspot_visuals(dm) +
+  labs(title = "DM Covid Death Hotpots")
+
+eu_death_hotspot <-
+  death_hotspot_visuals(eu) +
+  labs(title = "EU Covid Death Hotspots")
+
+asia_death_hotspot <-
+  death_hotspot_visuals(asia)+
+  labs(title = "Asia Covid Death Hotspots")
+
+latam_death_hotspot <-
+  death_hotspot_visuals(latam) +
+  labs(title = "Latam Covid Death Hotspots")
+
+global_death_hotspot <-
+  death_hotspot_visuals("global") +
+  labs(title = "Top 20 Global Covid Death Hotspots")
+
+
 # Define UI for app that draws a histogram ----
 ui <- fluidPage(
   
@@ -88,12 +278,11 @@ ui <- fluidPage(
     sidebarPanel(
       
       # Input: Slider for the number of bins ----
-      selectInput(inputId = "region",
-                  label = "Region",
-                  # choices = c( "em" = `em`, "dm" = `dm`, "asia" = `asia`, 
-                  #              "Latam" = `latam`, "EU" = `eu`)
-                  list(`EM` = "em", `DM` = "dm", `Asia` = "asia", 
-                       `Latam` = "latam", `EU` = "eu"))
+      selectInput(inputId = "type",
+                  label = "Covid Situation",
+                  list(`Cases` = "cases", `Death` = "deaths")
+                 ),
+      width = 1
       
     ),
     
@@ -101,109 +290,41 @@ ui <- fluidPage(
     mainPanel(
       
       # Output: Histogram ----
-      plotOutput(outputId = "distPlot")
+      plotOutput(outputId = "distPlot", width = "1600px", height = "850px"),
       
+      width = 11
     )
   )
 )
 
-# Define server logic required to draw a histogram ----
+
+#reactive output ---------------------------------------------------------
+
 server <- function(input, output) {
   
-  # Histogram of the Old Faithful Geyser Data ----
-  # with requested number of bins
-  # This expression that generates a histogram is wrapped in a call
-  # to renderPlot to indicate that:
-  #
-  # 1. It is "reactive" and therefore should be automatically
-  #    re-executed when inputs (input$bins) change
-  # 2. Its output type is a plot
   output$distPlot <- renderPlot({
     
-    hotspot <- 
-      cntry_cleaned_covid %>% 
-      group_by(country) %>% 
-      mutate(change_7day_avg_new_cases_per_pop = new_cases_avg_per_pop-lag(new_cases_avg_per_pop),
-             avg_chg_weeklycases = rollmean(change_7day_avg_new_cases_per_pop, k = 14, align = "right", fill = NA)) %>% 
-      filter(date == last(date)) %>%  
-      ungroup() %>% 
-      arrange(desc(avg_chg_weeklycases)) %>% 
-      select(country, avg_chg_weeklycases) 
-    
-    if (input$region == "em") {
+    if (input$type =="deaths") {
+
+      grid.arrange(em_death_hotspot, dm_death_hotspot, eu_death_hotspot, asia_death_hotspot, 
+                   latam_death_hotspot, global_death_hotspot,
+                   nrow = 2)
+     
       
-      ranked_data <- 
-        hotspot %>% 
-        filter(country %in% em) 
+    } else { 
       
-    } else if (input$region == "dm") {
       
-      ranked_data <- 
-        hotspot %>% 
-        filter(country %in% dm) 
+      grid.arrange(em_case_hotspot, dm_case_hotspot, eu_case_hotspot, asia_case_hotspot, 
+                   latam_case_hotspot, global_case_hotspot,
+                   nrow = 2)
       
-    }  else if (input$region == "eu") { 
-      
-      ranked_data <- 
-        hotspot %>% 
-        filter(country %in% eu) 
-      
-    } else if (input$region == "latam") {
-      
-      ranked_data <- 
-        hotspot %>% 
-        filter(country %in% latam) 
-      
-    } else if (input$region == "asia") {
-      
-      ranked_data <- 
-        hotspot %>% 
-        filter(country %in% asia) 
-      
-    } else {
-      
-      ranked_data <-
-        hotspot %>%
-        slice_head(n = 20)
-    }
-    
-    
-    ranked_list <- 
-      unique(ranked_data$country)
-    
-    hot <- 
-      ranked_data %>% 
-      mutate(country = fct_rev(factor(country, levels = ranked_list))) %>% 
-      filter(country != "NA") %>% 
-      mutate(avg_chg_weeklycases = ifelse(avg_chg_weeklycases >0, avg_chg_weeklycases, 0))
-    
-    cool <- 
-      ranked_data %>% 
-      mutate(country = fct_rev(factor(country, levels = ranked_list))) %>% 
-      filter(country != "NA") %>% 
-      mutate(avg_chg_weeklycases = ifelse(avg_chg_weeklycases <0, avg_chg_weeklycases, 0))
-    
-    
-    ggplot() +
-      geom_bar(data = hot, aes(y= country, x = avg_chg_weeklycases, fill = avg_chg_weeklycases>0),
-               stat = "identity", fill = "red") +
-      geom_bar(data = cool, aes(y= country, x = avg_chg_weeklycases, fill = avg_chg_weeklycases<=0),
-               stat = "identity", fill = "grey") +
-      geom_vline(xintercept = 0)+
-      labs(x= "", y= "", 
-           subtitle = paste0("Change in Weekly Average New Cases Per Million (14-day avg.) \nAs of ", today()-1)) +
-      theme_bw()+
-      theme(
-        plot.margin = margin(0, 1, 0.5, 1, unit = "cm"),
-        plot.subtitle= element_text(size = 16),
-        plot.title = element_text(size = 18),
-        axis.text = element_text(face = "bold", size = 18),
-        axis.text.y = element_text(face = "bold", size = 18,
-                                   color = as.vector((rev(ifelse(ranked_data$avg_chg_weeklycases >= 0, "red", "black"))))))
+      }
     
   })
   
 }
 
+
 # Create Shiny app ----
 shinyApp(ui = ui, server = server)
+
